a0e3eb39080fdb6c83993d97da36c8bbe19dc778
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-09-12 00:40:24 +0800

    Update Program - DSA argc, argv

--- DSA.c
+++ DSA.c
@@ -1,10 +1,32 @@
+/**
+ * @file DSA.c
+ * @author @Kenzouh @williebonavente
+ * @brief
+ * @version 0.1
+ * @date 2023-09-12
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include <stdio.h>
 
-int main (int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
 
     printf("\nHello, boi!\n\n");
     printf("GG!!\n");
+
+    /*
+    argc -> Argument Count, int numerical value.
+    argv -> Argument Vector, string
+    */
+    printf("\nYou have entered %d arguments.\n", argc);
+
+    for (int i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+    
+    printf("%n");
 
     return 0;
 }
