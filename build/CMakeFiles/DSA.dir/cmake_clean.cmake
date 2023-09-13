2bba502339f42e104a6c52f7a8954eb002136e30
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-09-13 21:35:55 +0800

    Add Cmake Constructing Repo

---
+++ build/CMakeFiles/DSA.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA.dir/Unit1/src/main.c.obj"
+  "CMakeFiles/DSA.dir/Unit1/src/main.c.obj.d"
+  "CMakeFiles/DSA.dir/Unit2/src/main.c.obj"
+  "CMakeFiles/DSA.dir/Unit2/src/main.c.obj.d"
+  "DSA.exe"
+  "DSA.exe.manifest"
+  "DSA.pdb"
+  "libDSA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/USER/OneDrive/Desktop/Temp/OpenSource/DSA-ken/DSA
+# Build directory: C:/Users/USER/OneDrive/Desktop/Temp/OpenSource/DSA-ken/DSA/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
